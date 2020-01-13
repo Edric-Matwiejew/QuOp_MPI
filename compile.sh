74e962f655b326ec400f06077f19b7bc751701e3
Edric <edric.matviev@gmail.com>
2020-01-13 14:15:02 +0800

    Compile command with debug flags.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+#python3 -m numpy.f2py --f90exec="mpifort" --f90flags="-Wall -Og -fcheck=all -fbacktrace" -c -L/usr/local/lib -lfftw3 -lfftw3_mpi -I/usr/local/include -m fqwao_mpi fqwao_mpi.f90
 python3 -m numpy.f2py --f90exec="mpifort" -c -L/usr/local/lib -lfftw3 -lfftw3_mpi -I/usr/local/include -m fqwao_mpi fqwao_mpi.f90
-#mpifort -w -pedantic -shared -fPIC -I/usr/local/include -o fqwao.so fqwao.f90 -L/usr/local/lib/ -lfftw3_mpi -lfftw3
