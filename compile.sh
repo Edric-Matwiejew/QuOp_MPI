f5c24dcae039d55e15619aa8d4686ce4181a985e
Edric <edric.matviev@gmail.com>
2020-01-14 23:23:21 +0800

    Build with parallel HDF5.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #python3 -m numpy.f2py --f90exec="mpifort" --f90flags="-Wall -Og -fcheck=all -fbacktrace" -c -L/usr/local/lib -lfftw3 -lfftw3_mpi -I/usr/local/include -m fqwao_mpi fqwao_mpi.f90
-python3 -m numpy.f2py --f90exec="mpifort" -c -L/usr/local/lib -lfftw3 -lfftw3_mpi -I/usr/local/include -m fqwao_mpi fqwao_mpi.f90
+python3 -m numpy.f2py --f90exec="mpifort" --f90flags="-Wall -Og -fcheck=all -fbacktrace" -c -L/home/edric/parallel-hdf5/lib -L/usr/local/lib -lhdf5_fortran -lfftw3 -lfftw3_mpi -I/usr/local/include -I/home/edric/parallel-hdf5/include -m fqwao_mpi fqwao_mpi.f90
