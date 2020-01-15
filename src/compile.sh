fcfd967b3ec9aa2632cc69090664f2c449c75c8c
Edric-Matwiejew <edric.matviev@gmail.com>
2020-01-15 21:59:00 +0800

    Replaced with makefile.

--- src/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#python3 -m numpy.f2py --f90exec="mpifort" --f90flags="-Wall -Og -fcheck=all -fbacktrace" -c -L/usr/local/lib -lfftw3 -lfftw3_mpi -I/usr/local/include -m fqwao_mpi fqwao_mpi.f90
-python3 -m numpy.f2py --f90exec="mpifort" --f90flags="-Wall -Og -fcheck=all -fbacktrace" -c -L$LIB -lhdf5_fortran -lfftw3 -lfftw3_mpi -I$INCLUDE -m fqwao_mpi fqwao_mpi.f90
