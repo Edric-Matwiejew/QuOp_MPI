122ca5e57b2417d16523abc205a575ea51df819a
Edric <edric.matviev@gmail.com>
2020-02-05 13:59:19 +0800

    Non-integer evenly distributed random qualities.

--- qwao_mpi/qualities.py
+++ qwao_mpi/qualities.py
@@ -28,6 +28,35 @@ def random_integers(N, local_i, local_i_offset, seed = 0):
 
     :param local_i_offset: Offset of the local QWAO state values relative to the zero index of the distributed array. Given by qwao.local_i_offset.
     :type local_i_offset: integer
+
+    :param seed: Integer to pass to np.random.seed(local_i_offset + seed).
+    :type seed: integer, optional, default = 0
     """
     np.random.seed(local_i_offset + seed)
     return np.random.randint(1, N + 1, size = local_i).astype(np.float64)
+
+def random_floats(N, local_i, local_i_offset, seed = 0, low = 0.0, high = 1.0):
+    """
+    Random floats evenly distributed between :math:`[low, high]`.
+
+    :param N: Size of the distrubted system.
+    :type N: integer
+
+    :param local_i: Number of local input QWAO state values, given by qwao.local_i.
+    :type local_i: integer
+
+    :param local_i_offset: Offset of the local QWAO state values relative to the zero index of the distributed array. Given by qwao.local_i_offset.
+    :type local_i_offset: integer
+
+    :param seed: Integer to pass to np.random.seed(local_i_offset + seed).
+    :type seed: integer, optional, default = 0
+
+    :param low: Lower bound.
+    :type low: float, optional, default = 0.0
+
+    :param high: Upper bound.
+    :type low: float, optional, default = 1.0
+
+    """
+    np.random.seed(local_i_offset + seed)
+    return np.random.uniform(low = low, high = high, size = local_i)
