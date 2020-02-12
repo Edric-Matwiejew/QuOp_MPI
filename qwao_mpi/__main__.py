3d892228176c6ea8703836d5ab4128d076907cf9
Edric-Matwiejew <edric.matviev@gmail.com>
2020-02-13 03:11:35 +0800

    Compatibilty.

--- qwao_mpi/__main__.py
+++ qwao_mpi/__main__.py
@@ -15,13 +15,12 @@ def x0(p):
 
 qwao = qw.MPI.qwao(n_qubits, comm)
 qwao.log_success("log", "qwao", action = "w")
-qwao.graph(qw.graph_array.circle(qwao.size))
+qwao.set_graph(qw.graph_array.circle(qwao.size))
 qwao.set_initial_state(name="split")
 qwao.set_qualities(qw.qualities.random_floats)
 qwao.plan()
 qwao.execute(x0(p))
 qwao.save("qwao", "example_config", action = "w")
-qwao.benchmark(x0,[1,2,4,6],3,early_stopping=True)
 qwao.destroy_plan()
 
 if comm.Get_rank() == 0:
@@ -35,8 +34,6 @@ qaoa.set_initial_state(name = "equal")
 qaoa.set_qualities(qw.qualities.random_floats)
 qaoa.execute(x0(p))
 qaoa.save("qaoa", "example_config", action = "w")
-
-qaoa.benchmark(x0,[1,2,4,6],3,early_stopping=True)
 
 if comm.Get_rank() == 0:
     print(qaoa.result)
