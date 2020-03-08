a76f88788b7d28635eba095a7af2ed4b0350cdd5
Edric-Matwiejew <edric.matviev@gmail.com>
2020-03-07 18:11:20 -0800

    Parallel write test program.

---
+++ src/compile_test.sh
@@ -0,0 +1,1 @@
+mpifort -L/usr/local/lib -I/usr/local/include -lhdf5_fortran -lfftw3 -lfftw3_mpi fqwoa_mpi_test.f90 -o test
---
+++ src/fqwoa_mpi_test.f90
@@ -0,0 +1,59 @@
+module fqwoa_mpi
+
+    contains
+
+    include 'fqwoa_mpi.f90'
+
+end module fqwoa_mpi
+
+program complex_write
+
+    use :: MPI
+    use :: fqwoa_mpi
+    use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64
+
+    implicit none
+
+    character(len = 128) :: file_name, group_name, dataset_name
+
+    integer(dp) :: N
+    integer(sp) :: local_i = 5, local_i_offset
+
+    complex(dp) :: complex_array(5)
+
+    integer :: i
+
+    integer :: flock
+    integer :: rank
+    integer :: ierr
+
+    call MPI_init(ierr)
+
+    file_name = "test_write"
+    group_name = "test_group"
+    dataset_name = "test_complex"
+
+    call MPI_comm_size(MPI_COMM_WORLD, flock, ierr)
+    call MPI_comm_rank(MPI_COMM_WORLD, rank, ierr)
+
+    N = flock * 5
+
+    local_i_offset = local_i * (rank - 1)
+
+    do i = 1, local_i
+        complex_array(i) = cmplx(i, i, dp)
+    enddo
+
+    call save_dist_complex( file_name, &
+                            group_name, &
+                            dataset_name, &
+                            "w", &
+                            N, &
+                            local_i, &
+                            local_i_offset, &
+                            complex_array, &
+                            MPI_COMM_WORLD)
+
+    call MPI_finalize(ierr)
+
+end program complex_write
