6dbd29241b76b8c9f24c084356693cd28bfcc4db
Edric-Matwiejew <edric.matviev@gmail.com>
2020-07-22 15:42:20 +0800

    Removed reference to *.md.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ EXTENSION = subprocess.run(
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, 'README.md'), encoding = 'utf-8') as f:
+with io.open(os.path.join(here, 'README.rst'), encoding = 'utf-8') as f:
     long_description = '\n' + f.read()
 
 setup(
