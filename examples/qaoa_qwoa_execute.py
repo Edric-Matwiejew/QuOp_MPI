0808c61a963747340902981a053499fd88b23e92
Ubuntu <edric.matviev@gmail.com>
2020-08-05 07:56:58 +0000

    Example code compatibility.

--- examples/qaoa_qwoa_execute.py
+++ examples/qaoa_qwoa_execute.py
@@ -1,5 +1,4 @@
 from mpi4py import MPI
-import networkx as nx
 import numpy as np
 import quop_mpi as qu
 
@@ -11,11 +10,10 @@ n_qubits = 7
 np.random.seed(2)
 
 def x0(p):
-    return np.random.uniform(low = 0, high = 1, size = 2 * p)
+    return np.random.uniform(low = 0, high = 2*np.pi, size = 2 * p)
 
 qwoa = qu.MPI.qwoa(n_qubits, comm)
 qwoa.log_results("log", "qwoa", action = "w")
-qwoa.set_graph(qu.graph_array.complete(qwoa.system_size))
 qwoa.set_initial_state(name="equal")
 qwoa.set_qualities(qu.qualities.random_floats)
 qwoa.plan()
@@ -24,8 +22,7 @@ qwoa.save("qwoa", "example_config", action = "w")
 qwoa.destroy_plan()
 qwoa.print_result()
 
-hyper_cube = nx.to_scipy_sparse_matrix(nx.hypercube_graph(n_qubits))
-qaoa = qu.MPI.qaoa(hyper_cube,comm)
+qaoa = qu.MPI.qaoa(n_qubits,comm)
 qaoa.log_results("log", "qaoa", action = "a")
 qaoa.set_initial_state(name = "equal")
 qaoa.set_qualities(qu.qualities.random_floats)
