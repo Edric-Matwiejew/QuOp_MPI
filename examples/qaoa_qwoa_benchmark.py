65e851ff6aba09b0e1d8cb401096928158671e16
Edric-Matwiejew <edric.matviev@gmail.com>
2020-11-22 21:47:44 +0800

    Moved tutorials to dedicated folder.

--- examples/qaoa_qwoa_benchmark.py
+++ examples/qaoa_qwoa_benchmark.py
@@ -1,4 +1,4 @@
-import quop_mpi as qw
+import quop_mpi as qu
 import numpy as np
 from mpi4py import MPI
 
@@ -10,20 +10,20 @@ def x0(p,seed):
     return rng.uniform(low = 0, high = 2*np.pi, size = 2 * p)
 
 for qubits in range(7,12):
-    qaoa = qw.MPI.qaoa(qubits,comm)
+    qaoa = qu.MPI.qaoa(qubits,comm)
     qaoa.set_initial_state(name = "equal")
     qaoa.log_results("benchmark_example","qaoa_equal",action="a")
-    qaoa.set_qualities(qw.qualities.random_floats)
+    qaoa.set_qualities(qu.qualities.random_floats)
     qaoa.benchmark(
             range(1,3),
             2,
             param_func = x0,
-            qual_func = qw.qualities.random_floats,
+            qual_func = qu.qualities.random_floats,
             filename = "qaoa_equal",
             label = "qaoa_" + str(qubits))
 
 for qubits in range(7,12):
-    qwoa = qw.MPI.qwoa(qubits,comm)
+    qwoa = qu.MPI.qwoa(qubits,comm)
     qwoa.set_initial_state(name="equal")
     qwoa.log_results("qwoa_complete_equal","equal",action="a")
     qwoa.plan()
@@ -31,7 +31,7 @@ for qubits in range(7,12):
             range(1,3),
             2,
             param_func = x0,
-            qual_func = qw.qualities.random_floats,
+            qual_func = qu.qualities.random_floats,
             filename = "qwoa_complete_equal",
             label = "qwoa_" + str(qubits))
     qwoa.destroy_plan()
