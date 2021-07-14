c3620fd79ce57e77c97e3832923faaddbe071e66
Edric-Matwiejew <edric.matviev@gmail.com>
2021-07-14 13:06:43 +0800

    Revised for API update.

--- examples/qaoa_qwoa_execute.py
+++ examples/qaoa_qwoa_execute.py
@@ -5,27 +5,25 @@ import quop_mpi as qu
 comm = MPI.COMM_WORLD
 
 p = 3
-n_qubits = 12
+n_qubits = 5
 
 rng = np.random.RandomState(1)
 
 def x0(p):
     return rng.uniform(low = 0, high = 2*np.pi, size = 2 * p)
 
-qwoa = qu.MPI.qwoa(n_qubits, comm)
-qwoa.log_results("log", "qwoa", action = "w")
+qwoa = qu.MPI.qwoa(n_qubits, comm, parallel = "jacobian")
+qwoa.set_log("log", "qwoa", action = "w")
 qwoa.set_initial_state(name="equal")
-qwoa.set_qualities(qu.qualities.random_floats)
-qwoa.plan()
+qwoa.set_observables(qu.qualities.random_floats)
 qwoa.execute(x0(p))
 qwoa.save("qwoa", "example_config", action = "w")
-qwoa.destroy_plan()
-qwoa.print_result()
+qwoa.print_optimiser_result()
 
-qaoa = qu.MPI.qaoa(n_qubits,comm)
-qaoa.log_results("log", "qaoa", action = "a")
+qaoa = qu.MPI.qaoa(n_qubits,comm, parallel = "jacobian")
+qaoa.set_log("log", "qaoa", action = "a")
 qaoa.set_initial_state(name = "equal")
-qaoa.set_qualities(qu.qualities.random_floats)
+qaoa.set_observables(qu.qualities.random_floats)
 qaoa.execute(x0(p))
 qaoa.save("qaoa", "example_config", action = "w")
-qaoa.print_result()
+qaoa.print_optimiser_result()
