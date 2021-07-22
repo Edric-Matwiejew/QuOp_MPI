8dcd85fae224284f3fcd49b115b08a242c021242
Edric-Matwiejew <edric.matviev@gmail.com>
2021-07-23 00:07:02 +0800

    Evolution benchmarks.

--- benchmarks/RUN_BENCHMARKS.sh
+++ benchmarks/RUN_BENCHMARKS.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-bash slurm_gen.sh 24 12:00:00 "float" "base.slurm" "qaoa" "..\/qaoa_benchmark.py"
-bash slurm_gen.sh 24 12:00:00 "float" "base.slurm" "qwoa" "..\/qwoa_benchmark.py"
+rm qwoa_evolution/*slurm
+rm qaoa_evolution/*slurm
 
-( cd float ; bash launch.sh )
+
+bash slurm_gen.sh "quop_mpi_evolution" 24 1 01:00:00 3600 "qwoa_evolution" "base.slurm" "qwoa_evolution.py" "pawsey0309"
+bash slurm_gen.sh "quop_mpi_evolution" 24 1 01:00:00 3600 "qaoa_evolution" "base.slurm" "qaoa_evolution.py" "pawsey0309"
+
+#( cd qwoa_evolution ; bash launch.sh )
+#( cd qaoa_evolution ; bash launch.sh )
---
+++ benchmarks/qwoa_evolution.py
@@ -0,0 +1,12 @@
+from quop_mpi.algorithms import qwoa
+from quop_mpi.operators import diagonal_uniform
+
+def function(system_size, COMM):
+    alg = qwoa(system_size, COMM)
+    alg.set_qualities(diagonal_uniform)
+    alg.set_depth(15)
+    params = alg.get_initial_params()
+    alg.pre()
+    alg.evolve_state(params)
+    alg.post()
+    return alg.final_state
