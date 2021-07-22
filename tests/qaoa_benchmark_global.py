d3b16223bfb92e0e54fe0c8ab80c1ff0a4034126
Edric-Matwiejew <edric.matviev@gmail.com>
2021-07-22 18:20:05 +0800

    integration tests

---
+++ tests/qaoa_benchmark_global.py
@@ -0,0 +1,26 @@
+from quop_mpi.algorithms import ansatz
+from mpi4py import MPI
+from quop_mpi.unitaries import diagonal, sparse
+from quop_mpi.states import equal
+from quop_mpi.operators import diagonal_uniform, sparse_hypercube
+from quop_mpi.params import uniform
+
+COMM = MPI.COMM_WORLD
+
+qubits = 4
+system_size = 2**qubits
+
+U1 = diagonal(diagonal_uniform, parameter_function = uniform)
+U2 = sparse(sparse_hypercube, parameter_function = uniform)
+
+qaoa = ansatz(system_size, COMM, parallel = "jacobian")
+qaoa.set_unitaries([U1, U2], 0)
+qaoa.set_log('test', 'test1')
+qaoa.benchmark(
+        range(1,4),
+        4,
+        param_persist = True,
+        verbose = True,
+        filename = 'test',
+        label = 'test',
+        save_action = 'a')
---
+++ tests/qaoa_class.py
@@ -0,0 +1,14 @@
+from mpi4py import MPI
+from quop_mpi.algorithms import qaoa
+from quop_mpi.operators import diagonal_uniform
+
+COMM = MPI.COMM_WORLD
+
+qubits = 4
+system_size = 2**4
+
+alg = qaoa(system_size, COMM)
+alg.set_qualities(diagonal_uniform)
+alg.set_depth(2)
+alg.execute()
+alg.print_optimiser_result()
