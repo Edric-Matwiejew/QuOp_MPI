456f2285233eff9bc3b2df8b543a7190a2bcfe51
Edric-Matwiejew <edric.matviev@gmail.com>
2021-07-23 17:31:32 +0800

    ansatz to __init__.py

--- quop_mpi/algorithms.py
+++ quop_mpi/algorithms.py
@@ -1,33 +1,7 @@
 from mpi4py import MPI
-from quop_mpi.__init__ import ansatz
+from quop_mpi.__init__ import phase_and_mixer
 
-class __phase_and_mixer(ansatz):
-
-    def __init__(self, system_size, MPI_communicator = MPI.COMM_WORLD, parallel = "global"):
-
-        super().__init__(system_size, MPI_communicator, parallel = "global")
-
-        self.operator_function = None
-        self.param_function = None
-
-    def set_qualities(self, operator_function, **kwargs):
-        self.operator_function = operator_function
-        self.operator_kwargs = kwargs
-
-    def set_params(self, param_function, **kwargs):
-        self.param_function
-        self.param_kwargs = kwargs
-
-    def _pre(self):
-
-        if self.operator_function is None:
-            raise RuntimeError("Rank {}: Solution qualities not defined.".format(self.rank))
-
-        if self.param_function is None:
-            from quop_mpi.params import uniform
-            self.set_params(uniform)
-
-class qwoa(__phase_and_mixer):
+class qwoa(phase_and_mixer):
 
     def pre(self):
 
@@ -50,7 +24,7 @@ class qwoa(__phase_and_mixer):
 
         super().pre()
 
-class qaoa(__phase_and_mixer):
+class qaoa(phase_and_mixer):
 
     def pre(self):
 
