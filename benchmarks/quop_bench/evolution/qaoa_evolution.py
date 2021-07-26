a88e4ca6c4faa15e48eb73abb4bfd799e6a35764
Edric-Matwiejew <edric.matviev@gmail.com>
2021-07-26 18:58:11 +0800

    removed un-needed 'pre' call

--- benchmarks/quop_bench/evolution/qaoa_evolution.py
+++ benchmarks/quop_bench/evolution/qaoa_evolution.py
@@ -1,12 +1,14 @@
+import copy
 from quop_mpi.algorithms import qaoa
 from quop_mpi.operators import diagonal_uniform
 
 def function(system_size, COMM):
-    alg = qaoa(system_size, COMM)
-    alg.set_qualities(diagonal_uniform)
-    alg.set_depth(15)
-    params = alg.get_initial_params()
-    alg.pre()
-    alg.evolve_state(params)
-    alg.post()
-    return alg.final_state
+    try:
+        alg = qaoa(system_size, COMM)
+        alg.set_qualities(diagonal_uniform)
+        alg.set_depth(15)
+        params = alg.get_initial_params()
+        alg.evolve_state(params)
+        return alg.local_i, copy.deepcopy(alg.final_state)
+    finally:
+        alg.post()
