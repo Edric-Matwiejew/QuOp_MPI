149b5118784254aa81424200beecbaf1048b860f
Edric-Matwiejew <edric.matviev@gmail.com>
2021-08-30 15:03:37 +0800

    use of set_parallel method

--- benchmarks/quop_bench/execute/qwoa_parallel_jacobian.py
+++ benchmarks/quop_bench/execute/qwoa_parallel_jacobian.py
@@ -3,7 +3,8 @@ from quop_mpi.operators.diagonal import random
 
 def function(system_size, depth, log_path, COMM):
     try:
-        alg = qwoa(system_size, COMM, parallel = 'jacobian')
+        alg = qwoa(system_size, COMM)
+        alg.set_parallel('jacobian')
         alg.set_log(log_path, 'qwoa parallel jacobian')
         alg.set_qualities(random)
         alg.set_depth(depth)
