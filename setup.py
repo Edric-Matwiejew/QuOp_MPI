31eda2ad6f1716e31804fb3652fa11ed932bf99e
Edric-Matwiejew <edric.matviev@gmail.com>
2021-09-19 22:45:02 +0800

    make external modules on install

--- setup.py
+++ setup.py
@@ -3,22 +3,56 @@
 
 import os
 import io
+import sys
 import subprocess
 
 from setuptools import find_packages, setup, Command
+from setuptools.command.build_ext import build_ext
+
+class Build(build_ext):
+
+    def parse_paths(paths, prefix):
+
+        existant = []
+        for path in paths.split(':'):
+            if os.path.exists(path):
+                existant.append(path)
+
+        for i in range(len(existant)):
+            existant[i] = ' {}{}'.format(prefix,existant[i])
+
+        return ''.join(existant)
+
+    LIB = os.environ.get('LIB')
+    INCLUDE = os.environ.get('INCLUDE')
+
+    if LIB is None:
+        LIB = "/usr/local/lib:/usr/lib:/usr/lib/x86_64-linux-gnu"
+    if INCLUDE is None:
+        INCLUDE = "/usr/local/include:/usr/include:/usr/include/x86_64-linux-gnu"
+
+    def run(self):
+
+        lib = parse_paths(LIB, '-L')
+        include = parse_paths(INCLUDE, '-I')
+
+        if subprocess.call("make -C src LIB=\"{}\" INCLUDE=\"{}\"".format(lib, include), shell = True) != 0:
+            sys.exit(-1)
+
+        build_ext.run(self)
 
 # Package meta-data.
 NAME = 'quop_mpi'
-DESCRIPTION = 'A framework for simulation of the QAOA algorithms.'
+DESCRIPTION = 'A framework for simulation of quantum variational algorithms.'
 URL = 'https://github.com/Edric-Matwiejew/QuOp_MPI'
 EMAIL = 'Edric.Matwiejew@research.uwa.au'
 AUTHOR = 'Edric Matwiejew'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.3'
+VERSION = '1.0.0'
 
 # What packages are required for this module to be exeuted?
 REQUIRED = [
-        'numpy', 'scipy', 'mpi4py', 'h5py', 'networkx', 'nlopt']
+        'numpy', 'scipy', 'mpi4py', 'h5py', 'nlopt', 'networkx', 'pandas', 'pandas-datareader' ]
 
 # What packages are optional?
 EXTRAS = {}
@@ -50,5 +84,8 @@ setup(
         install_requires = REQUIRED,
         extras_require = EXTRAS,
         license = 'GPLv3',
+        cmdclass={
+            'build_ext': Build,
+            }
         )
 
