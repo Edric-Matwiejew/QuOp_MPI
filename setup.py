4c31157b8f38740b3b027b5ed3e96e8f3afbc597
Edric-Matwiejew <edric.matviev@gmail.com>
2021-09-26 20:37:36 +0800

    environment variables set by config

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ import subprocess
 from setuptools import find_packages, setup, Command
 from setuptools.command.build_ext import build_ext
 
+
 class Build(build_ext):
 
     def run(self):
@@ -21,20 +22,19 @@ class Build(build_ext):
                     existant.append(path)
 
             for i in range(len(existant)):
-                existant[i] = ' {}{}'.format(prefix,existant[i])
+                existant[i] = " {}{}".format(prefix, existant[i])
 
-            return ''.join(existant)
+            return "".join(existant)
 
-        old_env = os.environ.copy()
+        lib = parse_paths(self.library_dirs, "-L")
+        include = parse_paths(self.include_dirs, "-I")
 
-        path_string = ""
-        for path in self.library_dirs:
-            path_string += "{}:".format(path)
-
-        lib = parse_paths(self.library_dirs, '-L')
-        include = parse_paths(self.include_dirs, '-I')
-
-        if subprocess.call("make -C src LIB=\"{}\" INCLUDE=\"{}\"".format(lib, include), shell = True) != 0:
+        if (
+            subprocess.call(
+                'make -C src LIB="{}" INCLUDE="{}"'.format(lib, include), shell=True
+            )
+            != 0
+        ):
             sys.exit(-1)
 
         os.environ.clear()
@@ -42,51 +42,62 @@ class Build(build_ext):
 
         build_ext.run(self)
 
+
 # Package meta-data.
-NAME = 'quop_mpi'
-DESCRIPTION = 'A framework for simulation of quantum variational algorithms.'
-URL = 'https://github.com/Edric-Matwiejew/QuOp_MPI'
-EMAIL = 'edric.matwiejew@research.uwa.au'
-AUTHOR = 'Edric Matwiejew'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0.0'
+NAME = "quop_mpi"
+DESCRIPTION = "A framework for simulation of quantum variational algorithms."
+URL = "https://github.com/Edric-Matwiejew/QuOp_MPI"
+EMAIL = "edric.matwiejew@research.uwa.au"
+AUTHOR = "Edric Matwiejew"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "1.0.0"
 
 # What packages are required for this module to be exeuted?
 REQUIRED = [
-        'numpy', 'scipy', 'mpi4py', 'h5py', 'nlopt', 'networkx', 'pandas', 'pandas-datareader' ]
+    "numpy",
+    "scipy",
+    "mpi4py",
+    "h5py",
+    "nlopt",
+    "networkx",
+    "pandas",
+    "pandas-datareader",
+]
 
 # What packages are optional?
 EXTRAS = {}
 
-EXTENSION = subprocess.run(
-        ['python3-config --extension-suffix'],
-        shell = True,
-        stdout = subprocess.PIPE).stdout.decode('utf-8').strip()
+EXTENSION = (
+    subprocess.run(
+        ["python3-config --extension-suffix"], shell=True, stdout=subprocess.PIPE
+    )
+    .stdout.decode("utf-8")
+    .strip()
+)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, 'README.rst'), encoding = 'utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
 setup(
-        name = NAME,
-        version = VERSION,
-        description = DESCRIPTION,
-        long_description = long_description,
-        long_description_content_type = 'text/markdown',
-        author = AUTHOR,
-        author_email = EMAIL,
-        python_requires = REQUIRES_PYTHON,
-        url = URL,
-        packages = find_packages(),
-        package_data = {
-            'quop_mpi': ['*' + EXTENSION],
-            },
-        install_requires = REQUIRED,
-        extras_require = EXTRAS,
-        license = 'GPLv3',
-        cmdclass={
-            'build_ext': Build,
-            }
-        )
-
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(),
+    package_data={
+        "quop_mpi": ["*" + EXTENSION],
+    },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    license="GPLv3",
+    cmdclass={
+        "build_ext": Build,
+    },
+)
