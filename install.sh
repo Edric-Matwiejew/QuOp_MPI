320fc3cafc8c1ebe5235b4cde3b8d0f5c28e6d1e
Edric-Matwiejew <edric.matviev@gmail.com>
2021-10-14 11:54:29 +0800

    HDF5_PKGCONFIG_NAME=hdf5-mpich

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # This script is an example of how to install and test
-# QuOp_MPI on a Debian-based system.
+# QuOp_MPI on a Debian-based system with mpich chosen
+# as the MPI implementation.
 
+# Install required packages from distro repositories:
 sudo apt-get update -qq && apt-get -y  --no-install-recommends install \
 build-essential \
 pkg-config \
@@ -13,23 +15,26 @@ libhdf5-mpich-dev \
 libfftw3-dev \
 libfftw3-mpi-dev
 
-# required
+# Install the required python modules:
 sudo python3 -m pip install setuptools
-sudo python3 -m pip install wheel numpy scipy mpi4py h5py
+sudo python3 -m pip install wheel numpy scipy mpi4py nlopt pandas
 
-# recommended
-sudo python3 -m pip install nlopt pandas
+# Importing a version of H5Py which is built against a different version of
+# HDF5 with cause a QuOp_MPI to crash when writing simulation results to disk.
+# Build h5py against the local version of HDF5:
+sudo CC="mpicc" HDF5_PKGCONFIG_NAME="hdf5-mpich" python3 -m pip -v install --no-cache --no-binary=h5py h5py
 
-# optional (to run examples)
+# Install optional python modules needed to run the examples:
 sudo python3 -m pip install pandas-datareader networkx
+
+# Build QuOp_MPI:
 sudo python3 setup.py sdist bdist_wheel
 
-# build the package and install QuOp_MPI
+# Install QuOp_MPI:
 cd dist
 sudo python3 -m pip install quop_mpi-1.0.0.tar.gz
-cd ../
 
-# try out an example
+# Test the installation by running an example:
+cd ../
 cd examples/maxcut
 python3 maxcut.py
-cd ../../
