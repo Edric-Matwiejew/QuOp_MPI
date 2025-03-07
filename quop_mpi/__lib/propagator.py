47319322b6e098992ec338a1d09efaed7d951e92
Edric <edric.matviev@gmail.com>
2025-03-07 09:44:48 +0800

    use np.intp for array pointer addresses

--- quop_mpi/__lib/propagator.py
+++ quop_mpi/__lib/propagator.py
@@ -1,26 +1,27 @@
 import numpy as np
 
 def array_list_to_pointers(arrays):
-
+    """
+    Convert a list of NumPy arrays into two Numpy arrays:
+    one containing the pointer addresses (as np.intp) and one with their sizes.
+    """
     ptrs = []
     array_sizes = []
-
     for array in arrays:
-        ptrs.append(array.ctypes.data)
-        array_sizes.append(len(array))
-
-    ptrs = np.array(ptrs, dtype = np.int64)
-    array_sizes = np.array(array_sizes, dtype = np.int64)
+        contiguous = np.ascontiguousarray(array)
+        ptrs.append(contiguous.ctypes.data)
+        array_sizes.append(contiguous.size)
+    ptrs = np.array(ptrs, dtype=np.intp)
+    array_sizes = np.array(array_sizes, dtype=np.int64)
     return ptrs, array_sizes
 
 class propagator:
+
     def __init__(self, propagator):
         self.propagator = propagator
         self.ptr = self.propagator.setup()
         self.initialised = True
 
-    #def __del__(self):
-    #    self.destroy()
 
     def destroy(self):
         if self.initialised:
@@ -29,7 +30,8 @@ class propagator:
 
     def max_comm_size(self, system_size, available_ranks, constraints, COMM):
         ptrs, array_sizes = array_list_to_pointers(constraints)
-        return self.propagator.max_comm_size(self.ptr, system_size, available_ranks, ptrs, array_sizes, COMM)
+        return self.propagator.max_comm_size(self.ptr, system_size, available_ranks,
+                                          ptrs, array_sizes, COMM)
 
     def plan(self, context):
         self.propagator.plan(self.ptr, context.ptr)
@@ -39,5 +41,5 @@ class propagator:
         self.propagator.gen_operator(self.ptr, ptrs, array_sizes)
 
     def propagate(self, ts):
-        ts = np.array(ts, dtype = np.float64)
-        self.propagator.propagate(self.ptr, ts)
+        ts_arr = np.ascontiguousarray(ts, dtype=np.float64)
+        self.propagator.propagate(self.ptr, ts_arr)
