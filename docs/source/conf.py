4671c30d5ade6265e2a0a5543e680dac67efa13b
Edric-Matwiejew <edric.matwiejew@research.uwa.edu.au.com>
2026-01-28 18:03:00 +0800

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ author = "Edric Matwiejew"
 autodoc_mock_imports = ["mpi4py", "h5py", "nlopt", "quop_mpi._lib"]
 
 # The full version, including alpha/beta/rc tags
-release = "1.4.0"
+release = "1.5.0"
 
 
 # -- General configuration ---------------------------------------------------
